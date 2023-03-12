aaffcf9eba089e5be31f035c08885bd41e6919c5
ericchan224 <chenxk_ericchan@163.com>
2023-03-12 16:13:08 +0800

    add merge sort

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
+import (
+	"fmt"
+	"io"
+)
+
+// Sayer 接口
+type Sayer interface {
+	say()
+}
+
+// Mover 接口
+type Mover interface {
+	move()
+}
+
+// 接口嵌套
+type animal interface {
+	Sayer
+	Mover
+}
+
+type dog struct {
+	name string
+}
+
+// 实现Sayer接口
+func (d dog) say() {
+	fmt.Printf("%s会叫汪汪汪\n", d.name)
+}
+
+// 实现Mover接口
+func (d dog) move() {
+	fmt.Printf("%s会动\n", d.name)
+}
+
 func main() {
-	// test
-	m := map[string]int64{}
-	m["a"] = 1
+	var _ io.Writer
+	var x Sayer
+	var y Mover
+	var z animal
+
+	var a = dog{name: "旺财"}
+	x = a
+	y = a
+	z = a
+	x.say()
+	y.move()
+	z.move()
 }
